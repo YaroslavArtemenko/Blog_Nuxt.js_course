8107b990f8191806032023810ae49747a7c39707
Yaros <Yaroslavhik@yandex.ua>
2021-12-14 19:35:40 +0200

    added component Intro to UI and changed about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -4,8 +4,9 @@
       <div class="container">
         <h1 class="title"> About my blog: </h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <img src="@/assets/img/about.jpg" alt="">
+        <Intro title="Lorem ipsum dolor sit amet"></Intro>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <img src="@/assets/img/about.jpg" alt="">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
     </section>
@@ -13,8 +14,14 @@
 </template>
 
 <script>
+
+import Intro from "@/components/UI/Intro";
+
 export default {
-  name: 'IndexPageAbout'
+  name: 'IndexPageAbout',
+  components: {
+    Intro
+  }
 }
 </script>
 
