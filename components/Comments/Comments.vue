cf5dba61f87ac63017fa269dc5ec4a611638e7be
Yaros <Yaroslavhik@yandex.ua>
2021-12-15 13:21:21 +0200

    added styles to comments

--- components/Comments/Comments.vue
+++ components/Comments/Comments.vue
@@ -1,6 +1,7 @@
 <template>
   <section v-if="comments" class="comments">
     <div class="container">
+      <h2 class="title">Comments: </h2>
       <div class="comment" v-for="comment in comments" :key="comment.name">
         <p class="name">{{ comment.name }}</p>
         <p class="text">{{ comment.text }}</p>
@@ -21,6 +22,20 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.comments {
+  margin: 30px auto;
+  text-align: center;
+}
+.comment {
+  padding: 20px;
+  width: 600px;
+  margin-bottom: 20px;
+  background-color: #ffffff;
+  .name {
+    margin-bottom: 12px;
+    font-size: 24px;
+    color: #5c4de7;
+  }
+}
 </style>
