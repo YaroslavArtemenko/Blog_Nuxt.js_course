b9ba240a909422a4c2cc0f51640727233b5a94b9
Yaros <Yaroslavhik@yandex.ua>
2021-12-15 13:04:41 +0200

    added form to new comment component

--- components/Comments/NewComment.vue
+++ components/Comments/NewComment.vue
@@ -1,13 +1,52 @@
 <template>
+  <section class="new-comment">
+    <div class="container">
+      <h2 class="title"> New Comment! </h2>
+      <form @submit.prevent="onSubmit" class="contact-form">
+        <label>Name: </label>
+        <input v-model="comment.name" type="text">
+        <label>Text: </label>
+        <textarea v-model="comment.text"></textarea>
 
+        <!--      button-->
+        <div class="controls">
+          <button class="btn btnPrimary"> Submit! </button>
+        </div>
+
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "NewComment"
+  name: "NewComment",
+  data () {
+    return {
+      comment: {
+        name: '',
+        text: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.comment)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.new-comment {
+  text-align: center;
+  .contact-form {
+    max-width: 600px;
+    margin: 30px auto;
+  }
+  .controls {
+    margin: 30px 0;
+  }
 
+}
 </style>
