a58de67f4e8713de927e5922e082a804aebd08e1
Yaros <Yaroslavhik@yandex.ua>
2021-12-15 10:57:32 +0200

    added intro to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
     <Promo></Promo>
+    <Intro title="My lasts posts: "></Intro>
   </div>
 </template>
 
 <script>
 import Promo from "@/components/Promo";
+import Intro from "@/components/UI/Intro";
 
 export default {
   name: 'IndexPage',
-  components: {Promo}
+  components: {
+    Promo,
+    Intro
+  }
 }
 </script>
 
