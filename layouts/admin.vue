5e055f24973eb981d2ef19ae511844fb4ec8140c
Yaros <Yaroslavhik@yandex.ua>
2021-12-18 13:35:09 +0200

    created admin layout and admin page

---
+++ layouts/admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <Intro title="Admin Page"></Intro>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/system/Header.vue'
+export default {
+  components: { Header }
+}
+</script>
---
+++ pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <PostsList :posts="posts"></PostsList>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexAdminPage',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: '1 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          img: 'https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0'
+        },
+        {
+          id: 2,
+          title: '2 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg'
+        },
+        {
+          id: 3,
+          title: '3 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          img: 'https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg'
+        }
+      ]
+    }
+  }
+}
+</script>
