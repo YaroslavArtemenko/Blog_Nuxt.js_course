3e31f8995ba6c69ec1769c677ff64cf41ea2a576
Yaros <Yaroslavhik@yandex.ua>
2021-12-20 14:30:07 +0200

    created new page for comments for admin

---
+++ pages/admin/comments/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <CommentTable
+    :thead="['Name', 'Text', 'Status', 'Change Status', 'Delete']"
+  >
+    <tbody slot="tbody">
+      <tr v-for="comment in comments" :key="comment.name">
+        <td>
+          <span> {{ comment.name }} </span>
+        </td>
+        <td>
+          <span> {{ comment.text }} </span>
+        </td>
+        <td>
+          <span v-if="comment.status" class="status true"> Publish </span>
+          <span v-else class="status false"> Hidden </span>
+        </td>
+      </tr>
+    </tbody>
+  </CommentTable>
+</template>
+
+<script>
+import CommentTable from "@/components/admin/CommentTable";
+
+export default {
+  name: "IndexAdminCommentsPage",
+  components: {CommentTable},
+  layout: 'admin',
+  data() {
+    return {
+      comments: [
+        {
+          name: 'Alex',
+          text: 'Lorem ipsum dolor sit amet, consectetur',
+          status: true
+        },
+        {
+          name: 'Yaro',
+          text: 'Lorem ipsum dolor sit amet, consectetur',
+          status: false
+        }
+      ]
+    }
+  },
+  methods: {
+    onSubmit (post) {
+      console.log('Post Editing!')
+      console.log(post)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
