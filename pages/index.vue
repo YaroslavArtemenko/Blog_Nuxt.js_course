9b4978f26b2f3a4701f57f7ba9121de7d3ae50e6
Yaros <Yaroslavhik@yandex.ua>
2021-12-20 15:46:32 +0200

    added asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -18,24 +18,20 @@ export default {
     Contacts
   },
   asyncData () {
-
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          posts: [
+            {
+              id: 1,
+              title: '1 post',
+              descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+              img: 'https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0'
+            }
+          ]
+        })
+      }, 1500)
+    })
   },
-  data() {
-    return {
-      posts: []
-    }
-  },
-  created() {
-    setTimeout(() => {
-      this.posts = [
-        {
-          id: 1,
-          title: '1 post',
-          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          img: 'https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0'
-        }
-      ]
-    }, 1500)
-  }
 }
 </script>
