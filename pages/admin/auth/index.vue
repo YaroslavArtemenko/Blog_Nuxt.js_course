4793045b1d25996244969a8e505a4e0fc4e037c3
Yaros <Yaroslavhik@yandex.ua>
2021-12-20 14:51:46 +0200

    added new page for auth of admin

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="auth">
+    <div class="container">
+      <form @submit.prevent="onSubmit" class="auth__form">
+
+        <AppInput type="password" v-model="user.email"> Login: </AppInput>
+        <AppInput type="email" v-model="user.password"> Password: </AppInput>
+
+        <div class="controls">
+          <AppButton> Login! </AppButton>
+        </div>
+
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "IndexAdminAuthPage",
+  layout: 'admin',
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.auth {
+  text-align: center;
+}
+.auth__form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 40vh;
+  input {
+    min-width: 460px;
+  }
+}
+</style>
