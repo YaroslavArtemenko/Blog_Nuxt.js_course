bf16e70a71029c2607da9480303b6b897f816d3b
Yaros <Yaroslavhik@yandex.ua>
2021-12-21 17:57:07 +0200

    created action for addPost with post-method

--- store/index.js
+++ store/index.js
@@ -10,7 +10,12 @@ export const mutations = {
 
 export const actions = {
   addPost ({commit}, post) {
-    console.log(post)
+    // console.log(post)
+    return axios.post('https://blog-nuxt-dbf4b-default-rtdb.firebaseio.com/posts.json', post)
+      .then(res => {
+        commit('addPost', post)
+      })
+      .catch(e => console.log(e))
   }
 }
 
