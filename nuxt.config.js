425be3092ca11b18a39342eba0491c99ea73a3b9
Yaros <Yaroslavhik@yandex.ua>
2021-12-23 15:50:53 +0200

    added routes with generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,21 +12,21 @@ export default {
       lang: 'en'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: 'yandex-verification', content: 'KEY_OR_SPECIAL_FILE_WITH_KEY_IN_HTML_IN_STATIC_DIRECTORY'},
-      { name: 'google-site-verification', content: 'KEY_OR_SPECIAL_FILE_WITH_KEY_IN_HTML_IN_STATIC_DIRECTORY'}
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'format-detection', content: 'telephone=no'},
+      {name: 'yandex-verification', content: 'KEY_OR_SPECIAL_FILE_WITH_KEY_IN_HTML_IN_STATIC_DIRECTORY'},
+      {name: 'google-site-verification', content: 'KEY_OR_SPECIAL_FILE_WITH_KEY_IN_HTML_IN_STATIC_DIRECTORY'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    { src: '~/assets/scss/main.scss', lang: 'scss' }
+    {src: '~/assets/scss/main.scss', lang: 'scss'}
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -38,8 +38,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -63,11 +62,19 @@ export default {
   },
   generate: {
     routes() {
-      return axios.get('https://my-api/users').then(res => {
-        return res.data.map(user => {
-          return '/users/' + user.id
+      return axios.get('https://blog-nuxt-dbf4b-default-rtdb.firebaseio.com/posts.json')
+        .then(res => {
+
+          //Get ID
+          const postsArray = []
+          for (let key in res.data) {
+            postsArray.push({...res.data[key], id: key})
+          }
+          //Routes
+          return postsArray.map(post => {
+            return '/blog/' + post.id
+          })
         })
-      })
     }
   }
 }
