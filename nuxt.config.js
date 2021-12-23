144d279bd260a274dfe25e06b74ce7d2aefeb061
Yaros <Yaroslavhik@yandex.ua>
2021-12-23 13:05:09 +0200

    changed nuxt.config.js and added import package for title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,18 @@
+import pkg from './package.json'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: pkg.name,
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
