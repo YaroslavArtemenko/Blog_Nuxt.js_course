f06af4dece51b323ee1c96d7fd02e6901df60a08
Yaros <Yaroslavhik@yandex.ua>
2021-12-24 17:04:33 +0200

    resolved problems, when page reload

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -1,3 +1,5 @@
 export default function (context) {
-  context.store.dispatch('initAuth')
+  if (process.client) {
+    context.store.dispatch('initAuth')
+  }
 }
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -7,6 +7,9 @@
 export default {
   name: 'IndexAdminPage',
   layout: 'admin',
+  mounted() {
+    this.$store.dispatch('nuxtServerInit')
+  },
   computed: {
     postsLoaded () {
       return this.$store.getters.getPostsLoaded
