7e69d64f8b22e8da42afc8abe01fb648ebce4f57
Yaros <Yaroslavhik@yandex.ua>
2021-12-24 16:52:54 +0200

    added middleware for check

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -21,7 +21,7 @@ import Header from '~/components/system/Header.vue'
 
 export default {
   components: {Header},
-  middleware: ['auth'],
+  middleware: ['auth-check','auth'],
   methods: {
     logoutUser() {
       this.$store.dispatch('logoutUser')
